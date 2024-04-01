325e98d3d1f865651bd34bc79d0e1526f18e2a9f
Mahfuzur Rahman Shabbir <mahfuzurrahmanshabbir@gmail.com>
2024-04-01 14:20:41 +0600

    update

--- index.js
+++ index.js
@@ -208,8 +208,8 @@ async function run() {
       }
     });
 
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
