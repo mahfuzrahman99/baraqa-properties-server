382ae61870112186df22d305c6da230b7061d082
mahfuzrahman99 <mahfuzurrahmanshabbir@gmail.com>
2024-04-03 11:24:31 +0600

    update

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
       }
     });
 
-    app.post("/api/login", async (req, res) => {
+    app.post("/login", async (req, res) => {
       const user = req.body;
       console.log(user);
       try {
